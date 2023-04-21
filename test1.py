b3ee439354c62d9b8a31e55d4a867e719c27f39e
nikhilbanta <nikhilbanta@gmail.com>
2023-04-21 16:09:19 +0530

    New C variable in new branch

--- test1.py
+++ test1.py
@@ -1,8 +1,12 @@
 #test adding in github
 a = [1,7,8,9,6,56,56,4634]
+c = []
 b = []
 for i in a:
     out = i*i
+    out2 = round((((i+1)^2)/3),3)
     b.append(out)
+    c.append(out2)
 print("Number list:",a)
 print("Square List:",b)
+print("Custom List:",c)
